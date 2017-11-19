d0a0ef748690ad1717c4ef123c92256781414b44
dikshaj09 <dikshaj09@gmail.com>
2017-11-19 22:34:57 +0530

    change in home component

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -23,14 +23,8 @@ export class HomeComponent  {
     private addNewSlide() {
          this.slides.push(
             {image:'assets/images/School2.JPG',text:'BMW 1'},
-<<<<<<< HEAD
             {image:'assets/images/school.JPG',text:'BMW 2'},
             {image:'assets/images/Principal.JPG',text:'BMW 3'}
-=======
-            {image:'assets/images/School.JPG',text:'BMW 2'},
-            {image:'assets/images/School.JPG',text:'BMW 5'},
-            {image:'assets/images/Principal.JPG',text:'BMW 6'}
->>>>>>> 1c7d0090b973a42596f45c15e0beaa58877ad211
         );
     }
 
