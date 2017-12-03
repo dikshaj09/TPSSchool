69528061993d6ab8bc28bba18d8ae2bc0ca6039d
dikshaj09 <dikshaj09@gmail.com>
2017-12-03 14:57:08 +0530

    new contactus

--- src/app/ContactUs/contactus.component.ts
+++ src/app/ContactUs/contactus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../../service/home.service'
 
 @Component({
     selector: 'app-contactus',
@@ -6,5 +7,20 @@ import { Component, OnInit } from '@angular/core';
   })
 
   export class ContactUsComponent{
-
+    schoolInfo :any =[];
+    lat: number = 18.4017;
+    lng: number = 77.099047;
+    zoom:number = 16;
+    
+  constructor(private homeservice:HomeService){}
+  
+    ngOnInit(){
+      this.getSchoolInfo();
+    }
+  
+    getSchoolInfo(){
+      this.schoolInfo=this.homeservice.getSchoolInfo();
+      console.log("schoolInfo",this.schoolInfo);
+    
+    }
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { HttpModule }    from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Home/home.component';
-import {AppRoutingModule} from './app.routing' 
+import {AppRoutingModule} from './app.routing' ;
+import { AgmCoreModule } from '@agm/core';
+
 
 import {HomeService} from '../service/home.service';
 import { CaraouselComponent } from './Home/caraousel.component';
@@ -19,7 +21,10 @@ import { ContactUsComponent } from './ContactUs/contactus.component';
     ContactUsComponent
   ],
   imports: [
-    BrowserModule,HttpModule,AppRoutingModule
+    BrowserModule,HttpModule,AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCMcAwLQL8Rw7rvCdAQyo-0NQbzaSDA7yM'
+    })
   ],
   providers: [HomeService],
   bootstrap: [AppComponent]
